44aa08127d5b29f2f9f9dd258ef21963f0595603
Aaron Dutton <aaron.dutton@logos.com>
2015-10-26 08:40:00 +0700

    Refactor storage namespace

--- js/storage.js
+++ js/storage.js
@@ -1,14 +1,17 @@
 "use strict";
 
-function Storage() {
-};
-
-Storage.load_data = function(world_name, data_name) {
-	var full_path = "worlds/" + world_name + "/" + data_name + ".yaml"
-	return JSON.parse(window.localStorage.getItem(full_path));
-};
-
-Storage.save_data = function(world_name, data_name, obj) {
-    var full_path = "worlds/" + world_name + "/" + data_name + ".yaml"
-    window.localStorage.setItem(full_path, JSON.stringify(obj));
-};
+var storage = (function() {
+	return {
+		loadData: function(world_name, data_name) {
+			var full_path = "worlds/" + world_name + "/" + data_name + ".yaml";
+			return JSON.parse(window.localStorage.getItem(full_path));
+		},
+		saveData: function(world_name, data_name, obj) {
+			var full_path = "worlds/" + world_name + "/" + data_name + ".yaml";
+			window.localStorage.setItem(full_path, JSON.stringify(obj));
+		},
+		clear: function() {
+			window.localStorage.clear();
+		}
+	};
+}());
--- js/world.js
+++ js/world.js
@@ -12,18 +12,18 @@ class World {
 };
 
 World.load = function(world_name) {
-	var sectors = Storage.load_data(world_name, "sectors");
+	var sectors = storage.loadData(world_name, "sectors");
 	/* if (!sectors)
 		sectors = generate_sectors();*/
-	var ports = Storage.load_data(world_name, "ports");
+	var ports = storage.loadData(world_name, "ports");
 	/*if (!ports)
 		ports = generate_ports(sectors);*/
 	// TODO: Figure this out
 	var stardock_location = 0;
-	var chat_log = Storage.load_data(world_name, "chatlog");
+	var chat_log = storage.loadData(world_name, "chatlog");
 	/*if (!chat_log)
 		chat_log = new ChatLog();*/
-	var players = Storage.load_data(world_name, "players");
+	var players = storage.loadData(world_name, "players");
 	if (!players)
 		players = [];
 
@@ -31,10 +31,10 @@ World.load = function(world_name) {
 };
 
 World.prototype.save = function() {
-	Storage.save_data(this.name, "sectors", this.sectors);
-	Storage.save_data(this.name, "ports", this.ports);
-	Storage.save_data(this.name, "chat_log", this.chat_log);
-	Storage.save_data(this.name, "players", this.players);
+	storage.saveData(this.name, "sectors", this.sectors);
+	storage.saveData(this.name, "ports", this.ports);
+	storage.saveData(this.name, "chat_log", this.chat_log);
+	storage.saveData(this.name, "players", this.players);
 }
 
 class Sector {
