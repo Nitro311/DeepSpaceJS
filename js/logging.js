75c2e172c791b66f14c1f6e02e58a8aef92a6bcf
Aaron Dutton <adutton@gmail.com>
2015-10-25 21:40:23 -0700

    Add basic logging module

---
+++ js/logging.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var logging = (function () {
+	var logDiv = $('<div class="logging" style="height: 200px; overflow: auto">');
+	
+	$('body').append(logDiv);
+	
+	return {
+		info: function(message) {
+			console.log(message);
+			logDiv.append($('<div/>').html(message).text() + '<br/>');
+			logDiv.stop().animate({
+				scrollTop: logDiv[0].scrollHeight
+			}, 800);
+		}
+	};
+}());
