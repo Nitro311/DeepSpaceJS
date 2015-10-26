c4aa8537a9b74f33a5ee427b9560b463cadc668a
Aaron Dutton <aaron.dutton@logos.com>
2015-10-26 09:05:00 +0700

    Create worldFactory and implement generateSectors

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
+/*jshint browser: true, devel: true, jquery: true, globalstrict: true*/
+/*globals easygui, World, worldFactory, notification, logging, storage*/
 "use strict";
 
-var world;
-
 function loadNewWorld() {
-	var createNewWorldDialog = function() {
+	var createNewWorldDialog = function () {
 		easygui.inputBox("What do you want to name your world", "Create New World", null, "example: Phobos", createNewWorld);
-	}
+	};
 
-	var worldChoices = World.getNames().slice(0);
+	var worldChoices = worldFactory.getNames().slice(0);
 
 	if (worldChoices.length === 0) {
 		createNewWorldDialog();
@@ -16,44 +16,43 @@ function loadNewWorld() {
 
 	worldChoices.push("New");
 
-	easygui.buttonBox("Choose a world to load:", "World Selector", worldChoices, function(index, value) {
+	easygui.buttonBox("Choose a world to load:", "World Selector", worldChoices, function (index, value) {
 		if (index === worldChoices.length - 1) {
 			createNewWorldDialog();
 		} else {
-			var loadedWorld = World.load(value);
+			var loadedWorld = worldFactory.load(value);
 
 			if (loadedWorld) {
 				notification.info('Loaded existing world: ' + value);
-				world = loadedWorld;
+				window.world = loadedWorld;
 			} else {
 				notification.danger('Could not find world to load it: ' + value);
 			}
 		}
 	});
-};
+}
 
 function createNewWorld(name) {
-	if (!name)
+	if (!name) {
 		return;
+	}
 
-	world = World.create(name);
+	window.world = worldFactory.create(name);
 
 	notification.info('Created new world: ' + name);
-};
+}
 
-var world;
-
-$(function() {
-	$('.menu__toggle-log-button').on('click', function() {
+$(function () {
+	$('.menu__toggle-log-button').on('click', function () {
 		logging.toggle();
 	});
 
-	$('.menu__clear-storage-button').on('click', function() {
+	$('.menu__clear-storage-button').on('click', function () {
 		logging.info("Clearing storage");
 		storage.clear();
 	});
 
-	$('.menu__load-new-world-button').on('click', function() {
+	$('.menu__load-new-world-button').on('click', function () {
 		logging.info("Loading new world");
 		loadNewWorld();
 	});
--- js/world.js
+++ js/world.js
@@ -1,3 +1,6 @@
+/*jshint browser: true, devel: true, jquery: true, globalstrict: true*/
+/*globals storage, logging*/
+/*exported worldFactory*/
 "use strict";
 
 class World {
@@ -11,63 +14,140 @@ class World {
 	}
 };
 
-World.getNames = function() {
-	return storage.loadData('*', 'worlds') || [];
-}
-
-World.exists = function(name) {
-	return $.inArray(name, World.getNames()) >= 0;
-}
-
-World.create = function(name) {
-	if (World.exists(name) || name === '*')
-		return;
-
-	// TODO: Implement this function
-	var sectors = null; //generate_sectors();
-	var ports = null; //generate_ports(sectors);
-	// TODO: Figure this out
-	var stardock_location = 0;
-	var chat_log = null; //new ChatLog();
-	var players = [];
-
-	var world = new World(name, sectors, ports, stardock_location, players, chat_log);
-	world.save();
-
-	// Add to global world list so it can be discovered later
-	var worldList = World.getNames().slice(0);
-	worldList.push(name);
-	storage.saveData('*', 'worlds', worldList);
-
-	return world;
-};
-
-World.load = function(name) {
-	if (!World.exists(name))
-		return;
-
-	var sectors = storage.loadData(name, "sectors");
-	var ports = storage.loadData(name, "ports");
-	// TODO: Figure this out
-	var stardock_location = 0;
-	var chat_log = storage.loadData(name, "chatlog");
-	var players = storage.loadData(name, "players");
-
-	return new World(name, sectors, ports, stardock_location, players, chat_log);
-};
-
 World.prototype.save = function() {
 	storage.saveData(this.name, "sectors", this.sectors);
 	storage.saveData(this.name, "ports", this.ports);
 	storage.saveData(this.name, "chat_log", this.chat_log);
 	storage.saveData(this.name, "players", this.players);
-}
+};
+
+var worldFactory = (function ($, storage, logging) {
+	var randomChoice = function (arr) {
+		return arr[Math.floor(arr.length * Math.random())];
+	};
+
+	var shuffle = function (array) {
+		var counter = array.length, temp, index;
+
+		// While there are elements in the array
+		while (counter > 0) {
+			// Pick a random index
+			index = Math.floor(Math.random() * counter);
+
+			// Decrease counter by 1
+			counter--;
+
+			// And swap the last element with it
+			temp = array[counter];
+			array[counter] = array[index];
+			array[index] = temp;
+		}
+
+		return array;
+	};
+
+	var generateSectors = function () {
+		logging.info('Generating sectors');
+		var sectors = {};
+		var new_pool = [];
+		var current_pool = [100];
+
+		for(var id = 100; id < 1000; id++) {
+			sectors[id] = new Sector('Sector ' + id, [], []);
+			if (id !== 100) {
+				new_pool.push(id);
+			}
+		}
+
+		new_pool = shuffle(new_pool);
+
+		// TODO: Sector names
+		//var sector_names = shuffle(...);
+
+		while (new_pool.length > 0) {
+			var new_id = new_pool[0];
+			var current_id = randomChoice(current_pool);
+
+			sectors[current_id].routes.push(new_id);
+			sectors[new_id].routes.push(current_id);
+			//sectors[new_id].name = sector_names[0];
+			//sector_names.splice(0, 1);
+
+			new_pool.splice(0, 1);
+			current_pool.push(new_id);
+		}
+
+		for (var r = 0; r < 50; r++) {
+			var from_id = randomChoice(Object.keys(sectors));
+			var to_id = randomChoice(Object.keys(sectors));
+
+			// Guarantee it is not a loopback
+			while (to_id !== from_id) {
+				to_id = randomChoice(Object.keys(sectors));
+			}
+
+			sectors[from_id].warps.push(to_id);
+		}
+
+		return sectors
+	};
+
+	var generatePorts = function (sectors) {
+		// TODO: Implement this function
+		logging.info('Generating ports');
+	};
+
+	return {
+		getNames: function() {
+			return storage.loadData('*', 'worlds') || [];
+		},
+		exists: function(name) {
+			return $.inArray(name, worldFactory.getNames()) >= 0;
+		},
+		create: function(name) {
+			if (worldFactory.exists(name) || name === '*') {
+				return;
+			}
+
+			var sectors = generateSectors();
+			var ports = generatePorts(sectors);
+			// TODO: Figure this out
+			var stardock_location = 0;
+			var chat_log = null; //new ChatLog();
+			var players = [];
+
+			var world = new World(name, sectors, ports, stardock_location, players, chat_log);
+			world.save();
+
+			// Add to global world list so it can be discovered later
+			var worldList = worldFactory.getNames().slice(0);
+			worldList.push(name);
+			storage.saveData('*', 'worlds', worldList);
+
+			return world;
+		},
+		load: function(name) {
+			if (!worldFactory.exists(name)) {
+				return;
+			}
+
+			var sectors = storage.loadData(name, "sectors");
+			var ports = storage.loadData(name, "ports");
+			// TODO: Figure this out
+			var stardock_location = 0;
+			var chat_log = storage.loadData(name, "chatlog");
+			var players = storage.loadData(name, "players");
+
+			return new World(name, sectors, ports, stardock_location, players, chat_log);
+		}
+	};
+}(jQuery, storage, logging));
 
 class Sector {
-	constructor(warps, routes, name) {
-		this.warps = warps;
-		this.routes = routes;
+	constructor(name, routes, warps) {
 		this.name = name;
+		this.routes = routes;
+		this.warps = warps;
 	}
 };
 
