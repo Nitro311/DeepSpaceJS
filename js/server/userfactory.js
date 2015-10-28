b7dfc2899614dab7d8192fb78e54e5b1adb332d0
Aaron Dutton <aaron.dutton@logos.com>
2015-10-27 17:32:49 -0700

    Add gravatar images

--- js/server/userfactory.js
+++ js/server/userfactory.js
@@ -12,7 +12,7 @@ var userFactory = (function ($, storage, logging) {
 		}
 		return {
 			email: source.email,
-			avatar: source.avatar || 'images/default-avatar.png'
+			avatar: source.avatar
 		};
 	};
 
@@ -30,14 +30,21 @@ var userFactory = (function ($, storage, logging) {
 				.Any(function (x) { return x.email === email; });
 		},
 		create: function(email, password) {
+			if (!email) {
+				return false;
+			}
+
+			email = email.trim().toLowerCase();
+
 			if (userFactory.exists(email)) {
 				return false;
 			}
 
 			var rawData = loadRaw();
 			var hash = md5(password);
+			var avatarHash = md5(email);
 
-			rawData.push({ email: email, passwordHash: hash, avatar: null });
+			rawData.push({ email: email, passwordHash: hash, avatar: 'http://www.gravatar.com/avatar/' + avatarHash + '.jpg?s=60&d=mm' });
 
 			storage.saveData('*', 'users', rawData);
 		},
